5f2c77b888edfd79b46886df853dee66669e1664
msquarex <mittal.madhav12@gmail.com>
2024-09-07 20:02:06 +0530

    error resolved

--- components/Login.tsx
+++ components/Login.tsx
@@ -62,7 +62,7 @@ export default function Login() {
           </Button>
         </form>
         <div className="text-sm text-center">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link href="/signup" className="text-emerald-600 hover:underline">
             Sign up
           </Link>
